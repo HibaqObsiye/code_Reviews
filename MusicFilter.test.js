64f081a3199822397b2967c865caa58fe21c22c9
hibaq obsiye <obsiyehibaq@gmail.com>
2023-01-20 15:16:55 +0000

    passing test for frequency less than or greater than 40

--- MusicFilter.test.js
+++ MusicFilter.test.js
@@ -8,4 +8,12 @@ describe("musicFilter", () => {
     it("returns higher value 1000", () => {
         expect(musicFilter(1000)).toEqual(1000)
     })
+
+    it("returns 40 if value is greater than 40 but less than the higher value", () => {
+        expect(musicFilter(99)).toEqual(40)
+    })
+
+    it("returns 1000 if value is greater than 1000 but less than the higher value", () => {
+        expect(musicFilter(10001)).toEqual(1000)
+    })
 })
