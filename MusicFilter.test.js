76ab81810dc257394e93e6dcd77ed22764c0203f
hibaq obsiye <obsiyehibaq@gmail.com>
2023-01-21 17:17:50 +0000

    passing array to check if music filtered for frequency at 40

--- MusicFilter.test.js
+++ MusicFilter.test.js
@@ -1,19 +1,23 @@
 const musicFilter = require('./lib/musicFilter.js')
 
 describe("musicFilter", () => {
-    it("return lower value 40", () =>{
-        expect(musicFilter(40)).toEqual(40)
-    })
+    // it("return lower value 40", () =>{
+    //     expect(musicFilter(40)).toEqual(40)
+    // })
 
-    it("returns higher value 1000", () => {
-        expect(musicFilter(1000)).toEqual(1000)
-    })
+    // it("returns higher value 1000", () => {
+    //     expect(musicFilter(1000)).toEqual(1000)
+    // })
 
-    it("returns 40 if value is greater than 40 but less than the higher value", () => {
-        expect(musicFilter(99)).toEqual(40)
-    })
+    // it("returns 40 if value is greater than 40 but less than the higher value", () => {
+    //     expect(musicFilter(99)).toEqual(40)
+    // })
 
-    it("returns 1000 if value is greater than 1000 but less than the higher value", () => {
-        expect(musicFilter(10001)).toEqual(1000)
+    // it("returns 1000 if value is greater than 1000 but less than the higher value", () => {
+    //     expect(musicFilter(10001)).toEqual(1000)
+    // })
+
+    it('returns an unmodified array if values are within the ranges', () => {
+        expect(musicFilter([60,40, 39],60,40)).toEqual([60,40, 40])
     })
 })
